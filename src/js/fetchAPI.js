6df9fe495d11a59b1f515fdbc474ee010c6690f5
MelnykY <106350059+MelnykY@users.noreply.github.com>
2023-02-02 18:25:46 +0200

    Update fetchAPI.js

--- src/js/fetchAPI.js
+++ src/js/fetchAPI.js
@@ -1,8 +1,8 @@
 import { API_KEY, BASE_URL, TREND_URL } from './/api-vars.js';
 
-const API_KEY = '8b20680c766bc5c37143e5d6b7aa7894';
-const BASE_URL = 'https://api.themoviedb.org/3';
-const TREND_URL = `${BASE_URL}/trending/movie/week`;
+//const API_KEY = '8b20680c766bc5c37143e5d6b7aa7894';
+//const BASE_URL = 'https://api.themoviedb.org/3';
+//const TREND_URL = `${BASE_URL}/trending/movie/week`;
 const options = {
   headers: {
     'Content-Type': 'application/json;charset=utf-8',
