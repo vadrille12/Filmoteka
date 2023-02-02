1ccf692139341d5e65bfa65a05e832536264189b
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2023-02-02 18:23:27 +0200

    +

---
+++ src/js/filmSearch.js
@@ -0,0 +1,7 @@
+//export filmSearch
+const form = document.querySelector('.header-form__input-wrap');
+const input = document.querySelector('.header-form__input');
+const buttonFilmSearch = document.querySelector('.header-form__button');
+
+
+
