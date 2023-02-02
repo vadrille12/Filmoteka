93d99c85aba809b0f98ff2cf652146aa960e6571
MaryShtayn <106402872+MaryShtayn@users.noreply.github.com>
2023-02-02 17:09:25 +0100

    updated js file api function

--- src/js/filmCard.js
+++ src/js/filmCard.js
@@ -1,17 +1,18 @@
 import { getTrendingMovies } from './fetchAPI';
-// import cards-container.html from './cards-container';
-const moviesEl = document.querySelector('.movie__cards');
+
+const moviesEl = document.querySelector('.cards__list');
+console.log(moviesEl);
 
 function showTrendingMovies(results) {
   const cardMarkup = results
     .map(
       ({ poster_path, title, genres, release_date, vote_average }) =>
-        `<div class="movie__card">
+        `<li class="movie__card">
       <img
         src="${poster_path}"
         class="movie__cover"
       />
-    </div>
+    </li>
     <div class="movie__info">
       <h2 class="movie__title">${title}</h2>
       <div class="movie__item">
