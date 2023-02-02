8afd8327b5fd6bc866bd014eafe524eb00918d23
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2023-02-02 18:39:25 +0200

    import

--- src/js/filmSearch.js
+++ src/js/filmSearch.js
@@ -1,4 +1,6 @@
 //export filmSearch
+//import { API_KEY, BASE_URL, TREND_URL, SEARCH_URL } from './api-vars.js';
+
 const form = document.querySelector('.header-form__input-wrap');
 const input = document.querySelector('.header-form__input');
 const buttonFilmSearch = document.querySelector('.header-form__button');
