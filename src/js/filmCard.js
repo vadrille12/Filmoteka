9d89ee464a358e0333cf8b63226989739cfc552f
MaryShtayn <106402872+MaryShtayn@users.noreply.github.com>
2023-02-03 16:01:16 +0100

    fixed js file filmCard

--- src/js/filmCard.js
+++ src/js/filmCard.js
@@ -1,54 +1,58 @@
 import { getTrendingMovies } from './fetchAPI';
+import { getGenres } from './fetchGenres';
 
 const moviesEl = document.querySelector('.cards__list');
 const form = document.querySelector('.header-form');
-// console.log(moviesEl);
-
-form.addEventListener('submit', onSubmit);
 
 let page = 1;
-let arr = getTrendingMovies().then(respData => respData.results);
-// console.log(arr);
 
-async function onSubmit(evt) {
+export async function showTrendingMovies(page = 1) {
   try {
-    evt.preventDefault();
-    page = 1;
-    searchQuery = evt.currentTarget.elements.searchQuery.value;
-    evt.target.reset();
-
-    const data = await getTrendingMovies();
-    showTrendingMovies(data.results);
+    const data = await getTrendingMovies(page);
+    console.log(data);
+    renderTrendingMovies(data.results);
   } catch (err) {
     console.error(err.message);
   }
 }
 
-export function showTrendingMovies(arr) {
-  try {
-    const cardMarkup = arr
-      .map(({ poster_path, title, genres, release_date, vote_average }) => {
-        const genreNames = genres.map(g => g.name).join(', ');
+showTrendingMovies().then(console.log);
+
+function renderTrendingMovies(data) {
+  // console.log(data);
+  const urlImage = 'https://image.tmdb.org/t/p/w500/';
+
+  const cardMarkup = data
+    .map(
+      ({
+        poster_path,
+        title,
+        genre_ids: genres,
+        release_date,
+        vote_average,
+      }) => {
+        const genresName = genres.map(genre =>
+          console.log(localStorage.getItem(genre))
+        );
+        // console.log(genres);
         return `<li class="movie__card">
         <img
-          src="${poster_path}"
+          src="${urlImage}${poster_path}"
           class="movie__cover"
         />
         <div class="movie__info">
         <h2 class="movie__title">${title}</h2>
         <div class="movie__item">
-          <h3 class="movie__category">${genreNames}</h3>
-          <h3 class="movie__year">${release_date}</h3>
-          <div class="movie__average">${vote_average}</div>
+          <h3 class="movie__category"></h3>
+          <h3 class="movie__year">${release_date.slice(0, 4)}</h3>
+          <div class="movie__average">${Math.ceil(vote_average)}</div>
         </div>
       </div>
       </li>
       `;
-      })
-      .join('');
+      }
+    )
+    .join('');
 
-    moviesEl.innerHTML = cardMarkup;
-  } catch (err) {
-    console.error(err.message);
-  }
+  moviesEl.innerHTML = cardMarkup;
 }
