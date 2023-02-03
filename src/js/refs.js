5ec6d854cfab803113c7f3f5ff3c1719efadc0b3
NatalieUSA <110269066+NatalieUSA@users.noreply.github.com>
2023-02-03 20:41:49 +0200

    Update refs.js

---
+++ src/js/refs.js
@@ -0,0 +1,10 @@
+export const refs = {
+  form: document.querySelector('.header-form'),
+  formFilm: document.querySelector('.header-form'),
+  watchedBtn: document.querySelector('.watched-list-btn'),
+  paginationEl: document.getElementById('tui-pagination-container'),
+  moviesGallery: document.querySelector('.cards__list'),
+
+  //===== будет удален - после переименования moviesGallery в рабочем файле JS
+  moviesEl: document.querySelector('.cards__list'),
+};
