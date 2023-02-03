ec6f7fd4f7162405c4579bd45bb559c65e7f381b
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2023-02-03 19:02:58 +0200

    push

--- src/js/filmCard.js
+++ src/js/filmCard.js
@@ -1,22 +1,18 @@
 import { getTrendingMovies } from './fetchAPI';
+import  onFormSubmit  from './filmSearch';
 
 const moviesEl = document.querySelector('.cards__list');
 const form = document.querySelector('.header-form');
 // console.log(moviesEl);
 
-form.addEventListener('submit', onSubmit);
+// form.addEventListener('submit', onSubmit);
 
 let page = 1;
 let arr = getTrendingMovies().then(respData => respData.results);
 // console.log(arr);
 
-async function onSubmit(evt) {
+async function onFormSubmit() {
   try {
-    evt.preventDefault();
-    page = 1;
-    searchQuery = evt.currentTarget.elements.searchQuery.value;
-    evt.target.reset();
-
     const data = await getTrendingMovies();
     showTrendingMovies(data.results);
   } catch (err) {
@@ -27,8 +23,8 @@ async function onSubmit(evt) {
 export function showTrendingMovies(arr) {
   try {
     const cardMarkup = arr
-      .map(({ poster_path, title, genres, release_date, vote_average }) => {
-        const genreNames = genres.map(g => g.name).join(', ');
+      .map(({ poster_path, title, release_date, vote_average }) => {
+        // const genreNames = genres.map(g => g.name).join(', ');
         return `<li class="movie__card">
         <img
           src="${poster_path}"
@@ -37,7 +33,7 @@ export function showTrendingMovies(arr) {
         <div class="movie__info">
         <h2 class="movie__title">${title}</h2>
         <div class="movie__item">
-          <h3 class="movie__category">${genreNames}</h3>
+          <h3 class="movie__category"></h3>
           <h3 class="movie__year">${release_date}</h3>
           <div class="movie__average">${vote_average}</div>
         </div>
@@ -46,7 +42,7 @@ export function showTrendingMovies(arr) {
       `;
       })
       .join('');
-
+      console.log(cardMarkup)
     moviesEl.innerHTML = cardMarkup;
   } catch (err) {
     console.error(err.message);
--- src/js/filmSearch.js
+++ src/js/filmSearch.js
@@ -1,17 +1,12 @@
 
 import { API_KEY, BASE_URL, TREND_URL, SEARCH_URL } from './api-vars.js';
-//import { getTrendingMovies } from './fetchAPI'
+import { getTrendingMovies } from './fetchAPI'
 import {showTrendingMovies } from './filmCard'
 
-    const options = {
-        method: "GET",
-        headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-        } 
-}
-export default async function getMovie() {
+
+export default async function getMovie(query, page) {
     try {
-        const response = await fetch(`${SEARCH_URL}?api_key=${API_KEY}`, options);
+        const response = await fetch(`${SEARCH_URL}?api_key=${API_KEY}&language=en-US&page=${page}&include_adult=false&query=${query}`);
         if (!response.ok) {
         throw new Error(response.statusText)
         }
@@ -21,28 +16,53 @@ export default async function getMovie() {
         console.error(); 
     }
 }
-getMovie()
 
 
-//const form = document.querySelector('.header-form');
-const input = document.querySelector('.header-form__input');
-const buttonFilmSearch = document.querySelector('.header-form__button');
-const moviesGallery = document.querySelector(".cards__list")
+const formFilm = document.querySelector('.header-form');
+const input = document.querySelector('.header-form__input')
+const moviesGallery = document.querySelector(".cards__list");
+
+formFilm.addEventListener('submit', onFormSubmit);
+let page = 1;
+let query = '';
 
 
-// formFilm.addEventListener('submit', onFormSubmit);
 
-// async function onFormSubmit(event) {
-//     try {
-//         event.preventDefault();
-//         page = 1
-//         searchQuery = event.currentTarget.elements.searchQuery.value.trim();
-//         event.target(reset);
-//         const dataFilm = await getMovie();
-//         showTrendingMovies(dataFilm.results);
-//     } catch (err) {
-//         console.error(err.message)
-//     }
+export default  async function onFormSubmit(event) {
+    try {
+        event.preventDefault();
+        page = 1;
+        query = event.currentTarget.searchQuery.value.trim();
+       
+        if (!query) {
+            moviesGallery.innerHTML = "";
+            return
+        }
     
-//}
+       showTrendingMovies(arr);
+        if (query.results.total_results === 0) {
+        invalidSearch();
+        return;
+        }
+    } catch (error) {
+      console.error()
+}
+} 
+// event.target(reset);
 
+const invalidSearch = function () {
+  const notification = `<p class="uncorrect-search">
+    Search result was NOT successful. Enter the correct movie name and try again!
+  </p>;`;
+ formFilm.insertAdjacentHTML('beforeend', notification);
+  const removeNotification = debounce(() => {
+    formFilm.lastElementChild.remove();
+  }, 2000);
+  removeNotification();
+};
+
+
+
+
+
+//  
