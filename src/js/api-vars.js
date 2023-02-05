d17489a29d941fd6b8b9e3a9d9a726b33a4e985b
NatalieUSA <110269066+NatalieUSA@users.noreply.github.com>
2023-02-05 07:56:24 +0200

    creates feth on movies Trailer
    для задания: FT-24 Реалізувати показ трейлеру фільма (мобілка, планшет, десктоп).

--- src/js/api-vars.js
+++ src/js/api-vars.js
@@ -2,4 +2,5 @@ const API_KEY = '8b20680c766bc5c37143e5d6b7aa7894';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const TREND_URL = `${BASE_URL}/trending/movie/week`;
 const SEARCH_URL = `${BASE_URL}/search/movie`;
-export { API_KEY, BASE_URL, TREND_URL, SEARCH_URL };
+const TRAILER_URL = `${BASE_URL}/movie`;
+export { API_KEY, BASE_URL, TREND_URL, SEARCH_URL, TRAILER_URL };
--- src/js/fetchAPI.js
+++ src/js/fetchAPI.js
@@ -1,4 +1,4 @@
-import { API_KEY, BASE_URL, TREND_URL } from './/api-vars.js';
+import { API_KEY, BASE_URL, TREND_URL, TRAILER_URL } from './/api-vars.js';
 import showTrendingMovies from './/filmCard';
 export async function fetchTrendingMovies(page = 1) {
   try {
@@ -25,3 +25,35 @@ export async function fetchTrendingMovies(page = 1) {
 //   const data = await fetchTrendingMovies();
 //   //   console.log(data);
 // })();
+
+//===============FETCH TRAILER
+export async function fetchTrailer(filmId) {
+  try {
+    const response = await fetch(
+      `${TRAILER_URL}/${filmId}/videos?api_key=${API_KEY}&language=en-US`
+    );
+    const respTrailer = await response.json();
+
+    return respTrailer;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+// export function getTrailer(id) {
+//   fetchTrailer(id).then(data => {
+//     console.log(data);
+//     console.log(data.results.length);
+
+//     if (data.results.length > 0) {
+//       window.open(
+//         `https://www.youtube.com/watch?v=${data.results[0].key}`,
+//         '_blank'
+//       );
+//     } else {
+//       console.log('Sorry, but there is no trailer for this movie');
+//     }
+//   });
+// }
+
+// getTrailer(346364);
