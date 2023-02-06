6087854310a5f886115e981a793b856d7b9c080d
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2023-02-06 13:10:16 +0200

    empty-library
    Функція закоментована, тому що при переході на сторінку Home не відмальовуються популярні фільми (((

---
+++ src/js/library.js
@@ -0,0 +1,14 @@
+// import { refs } from './refs';
+// import fotoUrl from '../images/movies.jpg'
+
+// const container = document.querySelector('.cards__container')
+
+// function emptyLibrary() {
+//   const markup = `<div class="empty-library">
+//   <p class="empty-library__title">Your library is empty!</p>
+//   <img class="empty-library__img"  src="${fotoUrl}" " alt ="стрічка фільму" width="780" >
+//   </div> `
+//   container.innerHTML = markup ;
+//   refs.paginationEl.style.display = 'none';
+// }
+//emptyLibrary()
