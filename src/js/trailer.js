6fad56ca95a0a844c49a54a720298dd73ce0dca2
NatalieUSA <110269066+NatalieUSA@users.noreply.github.com>
2023-02-06 19:29:27 +0200

    creates button trailer on modal window
    add button - src\js\modalMovie.js
    add styles - src\sass\components\_modal-movie.scss
    update - src\js\trailer.js

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -5,12 +5,15 @@ import {
   SEARCH_URL,
   TRAILER_URL,
 } from './api-vars.js';
-import { fetchTrailer } from './fetchAPI.js';
-export function getTrailer(id) {
-  fetchTrailer(id).then(data => {
-    console.log(data);
-    console.log(data.results.length);
 
+import { refs } from './refs';
+import { fetchTrailer } from './fetchAPI.js';
+
+export async function getTrailer(id) {
+  try {
+    const data = await fetchTrailer(id);
+    // console.log(data);
+    // console.log(data.results.length);
     if (data.results.length > 0) {
       window.open(
         `https://www.youtube.com/watch?v=${data.results[0].key}`,
@@ -19,7 +22,8 @@ export function getTrailer(id) {
     } else {
       console.log('Sorry, but there is no trailer for this movie');
     }
-  });
+  } catch (err) {
+    console.error(err.message);
+  }
 }
-
-// getTrailer(346364);
+// getTrailer(4564);
