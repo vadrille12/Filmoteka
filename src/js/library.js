fa822b41d648aa3fc3b4bbb6a8ca747b5b1b866c
LianaSher <sheremetliana@gmail.com>
2023-02-08 12:01:58 +0200

    fix library

--- src/js/library.js
+++ src/js/library.js
@@ -2,13 +2,12 @@ import './switchDayNight';
 // import './watched';
 import fotoUrl from '../images/movies.png';
 
-const container = document.querySelector('.empty-library');
-emptyLibrary();
+const containerTravolta = document.querySelector('.empty-library');
 
-function emptyLibrary() {
+export function emptyLibrary() {
   const markup = `<div class="empty-library">
   <p class="empty-library__title">WTF!?</p>
   <img class="empty-library__img" src="${fotoUrl}" alt ="стрічка фільму" width="780" >
   </div> `;
-  container.insertAdjacentHTML('beforeend', markup);
+  containerTravolta.insertAdjacentHTML('beforeend', markup);
 }
